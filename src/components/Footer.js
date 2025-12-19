b40cbf3e0f979c28c7f30cbfcc4c579b870e253a
KhanhNguyen03 <khanhnguyn2528@gmail.com>
2025-12-19 09:50:01 +0700

    fix ui

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,85 +1,159 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faMapMarkerAlt, faPhone, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faMapMarkerAlt,
+  faPhone,
+  faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
     <footer className="footer ftco-section">
       <div className="container">
         <div className="row">
+          {/* Company Info */}
           <div className="col-md-6 col-lg-3 mb-4 mb-md-0">
-            <h2 className="footer-heading">Cleaning Company ddđ</h2>
+            <h2 className="footer-heading">Homehelper company</h2>
             <p>
-              A small river named Duden flows by their place and supplies it with the necessary regelialia.
-              It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
+              Chúng tôi cung cấp dịch vụ vệ sinh chuyên nghiệp, uy tín và chất
+              lượng cao. Đội ngũ nhân viên tận tâm, giàu kinh nghiệm, luôn sẵn
+              sàng phục vụ khách hàng.
             </p>
             <ul className="ftco-footer-social list-unstyled float-md-left float-lft mt-4">
               <li className="ftco-animate">
-                <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+                <a href="#">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
               </li>
               <li className="ftco-animate">
-                <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
+                <a href="#">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
               </li>
               <li className="ftco-animate">
-                <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+                <a href="#">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </a>
               </li>
             </ul>
           </div>
+
+          {/* Latest News */}
           <div className="col-md-6 col-lg-3 mb-4 mb-md-0">
-            <h2 className="footer-heading">Latest News</h2>
+            <h2 className="footer-heading">Tin Mới Nhất</h2>
+
             <div className="block-21 mb-4 d-flex">
-              <a className="img mr-4 rounded" style={{ backgroundImage: "url('/images/image_1.jpg')" }}></a>
+              <a
+                className="img mr-4 rounded"
+                style={{ backgroundImage: "url('/images/image_1.jpg')" }}
+              ></a>
               <div className="text">
                 <h3 className="heading">
-                  <a href="#">Even the all-powerful Pointing has no control about</a>
+                  <a href="#">
+                    Mẹo vệ sinh nhà cửa hiệu quả và tiết kiệm thời gian
+                  </a>
                 </h3>
                 <div className="meta">
-                  <div><a href="#"><span className="icon-calendar"></span> Mar. 04, 2020</a></div>
-                  <div><a href="#"><span className="icon-person"></span> Admin</a></div>
-                  <div><a href="#"><span className="icon-chat"></span> 19</a></div>
+                  <div>
+                    <a href="#">04 Tháng 03, 2020</a>
+                  </div>
+                  <div>
+                    <a href="#">Quản trị viên</a>
+                  </div>
+                  <div>
+                    <a href="#">19</a>
+                  </div>
                 </div>
               </div>
             </div>
+
             <div className="block-21 mb-4 d-flex">
-              <a className="img mr-4 rounded" style={{ backgroundImage: "url('/images/image_2.jpg')" }}></a>
+              <a
+                className="img mr-4 rounded"
+                style={{ backgroundImage: "url('/images/image_2.jpg')" }}
+              ></a>
               <div className="text">
                 <h3 className="heading">
-                  <a href="#">Even the all-powerful Pointing has no control about</a>
+                  <a href="#">Dịch vụ vệ sinh chuyên nghiệp cho gia đình</a>
                 </h3>
                 <div className="meta">
-                  <div><a href="#"><span className="icon-calendar"></span> Mar. 04, 2020</a></div>
-                  <div><a href="#"><span className="icon-person"></span> Admin</a></div>
-                  <div><a href="#"><span className="icon-chat"></span> 19</a></div>
+                  <div>
+                    <a href="#">04 Tháng 03, 2020</a>
+                  </div>
+                  <div>
+                    <a href="#">Quản trị viên</a>
+                  </div>
+                  <div>
+                    <a href="#">19</a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+
+          {/* Quick Links */}
           <div className="col-md-6 col-lg-3 pl-lg-5 mb-4 mb-md-0">
-            <h2 className="footer-heading">Quick Links</h2>
+            <h2 className="footer-heading">Liên Kết Nhanh</h2>
             <ul className="list-unstyled">
-              <li><Link to="/" className="py-1 d-block">Home</Link></li>
-              <li><Link to="/about" className="py-1 d-block">About</Link></li>
-              <li><Link to="/services" className="py-1 d-block">Services</Link></li>
-              <li><Link to="/portfolio" className="py-1 d-block">Works</Link></li>
-              <li><Link to="/blog" className="py-1 d-block">Blog</Link></li>
-              <li><Link to="/contact" className="py-1 d-block">Contact</Link></li>
-              <li><Link to="/terms-of-service" className="py-1 d-block">Điều khoản sử dụng</Link></li>
+              <li>
+                <Link to="/" className="py-1 d-block">
+                  Trang chủ
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="py-1 d-block">
+                  Giới thiệu
+                </Link>
+              </li>
+              <li>
+                <Link to="/services" className="py-1 d-block">
+                  Dịch vụ
+                </Link>
+              </li>
+              <li>
+                <Link to="/portfolio" className="py-1 d-block">
+                  Dự án
+                </Link>
+              </li>
+              <li>
+                <Link to="/blog" className="py-1 d-block">
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact" className="py-1 d-block">
+                  Liên hệ
+                </Link>
+              </li>
+              <li>
+                <Link to="/terms-of-service" className="py-1 d-block">
+                  Điều khoản sử dụng
+                </Link>
+              </li>
             </ul>
           </div>
+
+          {/* Contact */}
           <div className="col-md-6 col-lg-3 mb-4 mb-md-0">
-            <h2 className="footer-heading">Have a Questions?</h2>
+            <h2 className="footer-heading">Bạn Có Câu Hỏi?</h2>
             <div className="block-23 mb-3">
               <ul>
                 <li>
                   <FontAwesomeIcon icon={faMapMarkerAlt} />
-                  <span className="text">Khu đô thị FPT City, Ngũ Hành Sơn, Đà Nẵng 550000, Việt Nam</span>
+                  <span className="text">
+                    Khu đô thị FPT City, Ngũ Hành Sơn, Đà Nẵng 550000, Việt Nam
+                  </span>
                 </li>
                 <li>
-                  <a href="tel:+23923929210">
+                  <a href="tel:+0906070605">
                     <FontAwesomeIcon icon={faPhone} />
-                    <span className="text">+0906070605</span>
+                    <span className="text">+0906 070 605</span>
                   </a>
                 </li>
                 <li>
@@ -92,12 +166,20 @@ const Footer = () => {
             </div>
           </div>
         </div>
+
+        {/* Copyright */}
         <div className="row mt-5">
           <div className="col-md-12 text-center">
             <p className="copyright">
-              Copyright &copy;{new Date().getFullYear()} All rights reserved | This template is made with{' '}
-              <i className="fa fa-heart" aria-hidden="true"></i> by{' '}
-              <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib.com</a>
+              © {new Date().getFullYear()} Bản quyền thuộc về Công ty Vệ Sinh
+              ddd | Giao diện được thiết kế bởi{" "}
+              <a
+                href="https://colorlib.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Colorlib
+              </a>
             </p>
           </div>
         </div>
@@ -106,4 +188,4 @@ const Footer = () => {
   );
 };
 
-export default Footer; 
+export default Footer;
